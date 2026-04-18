de16b2a90781bdff42ccdd50497a537433b8c8b2
Meepster27 <sharpieisme27@gmail.com>
2026-04-18 13:41:57 -0500

    app.js

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { SafeAreaView, StatusBar, Platform, View } from "react-native";
 import PlottingOverlays from "./PlottingOverlays";
+import styles from "./styles";
 
 export default function App() {
-  return <PlottingOverlays />;
+  useEffect(() => {
+    // Configure status bar for Android
+    if (Platform.OS === "android") {
+      StatusBar.setBarStyle("dark-content");
+      StatusBar.setBackgroundColor("#ffffff");
+    }
+  }, []);
+
+  return (
+    <SafeAreaView style={[styles.container, { flex: 1 }]}>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+      <View style={{ flex: 1 }}>
+        <PlottingOverlays />
+      </View>
+    </SafeAreaView>
+  );
 }
